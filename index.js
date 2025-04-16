deebe05c9bbdf18ddb6cdd0e2e9cbe7c81a8349a
gobrat321 <gobratgamalpok@gmail.com>
2025-04-16 15:14:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
